e4700e9ca900873a2e63f803cf2e51c65330eee0
fzk <0>
2023-05-10 10:00:12 +0800

    first

---
+++ BookingRoomBackend/Controllers/LoginInController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingRoomBackend.Controllers
+{
+    [ApiController]
+    public class LoginInController : ControllerBase
+    {
+        [HttpGet("login")]
+        public async Task<IActionResult> GetLoginIn(string userName, string password)
+        {
+            if (userName == "admin" && password == "admin") 
+            {
+                return this.Ok();
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ BookingRoomBackend/LoginIn.cs
@@ -0,0 +1,8 @@
+﻿namespace BookingRoomBackend
+{
+    public class LoginIn
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
